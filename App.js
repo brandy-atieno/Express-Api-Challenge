0a5ed1448f26119c29ef69cb5b0778d6f8ff7642
Brandy Odhiambo <71652231+brandy-atieno@users.noreply.github.com>
2022-06-28 15:57:50 +0300

    "Added Error Handling"

--- App.js
+++ App.js
@@ -7,7 +7,20 @@ server.use(bodyparser.urlencoded({extended: true}));
 const {router}=require('./routes/users')
 
 server.use('/', router)
-
+app.use((req,res,next)=>{
+    const error=new  Error ('NOT FOUND');
+    error.status(404);
+    next(error);
+})
+app.use((error,req,res,next) => {
+            res.status( error.status || 500);
+            res.json({ 
+                error:
+                {
+                message:error.message}
+            })
+        });
+                      
 
 
 
